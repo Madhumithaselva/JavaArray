f2b03eb0dc806b9aead83bf0dba389208025ccdf
Madhu <madhu8129@gmail.com>
2023-11-27 14:03:31 +0100

    Ex1 in Array exercise created

---
+++ src/main/java/se/lexicon/Ex1IntArray.java
@@ -0,0 +1,15 @@
+package se.lexicon;
+
+public class Ex1IntArray {
+    public static void main(String[] args){
+        //Declare an array
+        int[] intArray={11, 23, 49};
+
+        //Print the array elements
+        System.out.println("Array elements are:");
+        for(int i=0;i< intArray.length;i++){
+            System.out.print(intArray[i]+" ");
+        }
+
+    }
+}
